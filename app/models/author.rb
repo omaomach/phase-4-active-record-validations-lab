c0def654a1da79642b8003eac416e7bf42e09037
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-11-23 07:34:28 +0300

    completed phase 4 active record validations lab

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    # validates_uniqueness_of :name, :case_sensitive => true
+    validates :phone_number, length: { is: 10 }
+
 end
